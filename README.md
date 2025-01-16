dc4ec63a01f8043a087a017bc7e1871e194802a7
vanJker <vanJker@qq.com>
2025-01-16 16:09:42 +0800

    Enable paging

--- README.md
+++ README.md
@@ -16,5 +16,8 @@ $ make run
 Boot and load tp5a0 kernel, also enable protected mode and paging. 
 
 - boot: In the 0th sector (LBA), and be loaded to memory address 0x7c00 ~ 0x7dff, size of 512B.
-- loader: In the 2nd ~ 4th sectors (LBA), and be loaded to memory 0x900 ~ 0x10ff, size of 2KB.
+- loader: In the $[2nd, 4th]$ sectors (LBA), and be loaded to memory 0x900 ~ 0x10ff, size of 2KB.
 
+### Memory
+
+Page directory locates in physical address 0x100000 (1MB) with size 4K, first page table locates in physical address 0x101000 (1MB + 4KB) with size 4KB.
--- src/bootloader/loader.asm
+++ src/bootloader/loader.asm
@@ -13,10 +13,9 @@ SECTION LOADER vstart=LOADER_BASE_ADDR
 
     ; detect memory capacity
     call detect_memory
-    xchg bx, bx
 
-    ; Enter protected mode
-    ; 1. Enable A20
+    ; enter protected mode
+    ; 1. enable A20
     in al, 0x92
     or al, 0x02
     out 0x92, al
@@ -24,7 +23,7 @@ SECTION LOADER vstart=LOADER_BASE_ADDR
     ; 2. load GDT to GDTR
     lgdt [GDT_PTR]
 
-    ; 3. Enable CR0's PE flag
+    ; 3. enable cr0's PE flag
     mov eax, cr0
     or eax, 0x00000001
     mov cr0, eax
@@ -165,7 +164,112 @@ protected_mode_start:
     mov byte [gs:186], 'e'
     mov byte [gs:187], 0xed
 
+    ; enable paging
+    ; 1. setup page directory and page table
+    call setup_page_table
+
+    ; 2. reset address in GDT and GDTR to be virtual address
+    sgdt [GDT_PTR]
+    mov ebx, [GDT_PTR + 2]                  ; get base address of GDT
+    or dword [ebx + 0x18 + 4], 0xC0000000   ; map high 4 byet of base address of video segment to kernel space
+                                            ; since low 4 byte is under 1MB
+    add dword [GDT_PTR + 2], 0xC0000000     ; map base address of GDT to kernel space
+
+    ; 3. reset stack pointer to be virtual address
+    add esp, 0xC0000000
+
+    ; 4. setup cr3 (PDR3)
+    mov eax, PAGE_DIR_ADDR
+    mov cr3, eax
+
+    ; 5. enable cr0's PG flag
+    mov eax, cr0
+    or eax, (0x1 << 31)
+    mov cr0, eax
+
+    ; 6. reload GDTR and GDT which reset by virtual address
+    lgdt [GDT_PTR]
+
+    ; print string by modify display memory
+    mov byte [gs:320], 'V'
+    mov byte [gs:321], 0xfe
+    mov byte [gs:322], 'i'
+    mov byte [gs:323], 0xfe
+    mov byte [gs:324], 'r'
+    mov byte [gs:325], 0xfe
+    mov byte [gs:326], 't'
+    mov byte [gs:327], 0xfe
+    mov byte [gs:328], 'u'
+    mov byte [gs:329], 0xfe
+    mov byte [gs:330], 'a'
+    mov byte [gs:331], 0xfe
+    mov byte [gs:332], 'l'
+    mov byte [gs:333], 0xfe
+    mov byte [gs:334], ' '
+    mov byte [gs:335], 0xfe
+    mov byte [gs:336], 'm'
+    mov byte [gs:337], 0xfe
+    mov byte [gs:338], 'e'
+    mov byte [gs:339], 0xfe
+    mov byte [gs:340], 'm'
+    mov byte [gs:341], 0xfe
+    mov byte [gs:342], 'o'
+    mov byte [gs:343], 0xfe
+    mov byte [gs:344], 'r'
+    mov byte [gs:345], 0xfe
+    mov byte [gs:346], 'y'
+    mov byte [gs:347], 0xfe
+
+    xchg bx, bx
+
     jmp $
+
+; Setup page directory and page table about 1MB memory space in 32-bit mode
+; @param    void
+; @return   void
+setup_page_table:
+    mov ecx, 0x1000 ; 4KB
+    mov esi, 0
+.clear_page_dir:
+    mov byte [PAGE_DIR_ADDR + esi], 0
+    inc esi
+    loop .clear_page_dir
+
+    ; create pdes about low 1MB of physical memory
+    mov eax, PAGE_TBL_ADDR
+    mov ebx, eax
+
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov [PAGE_DIR_ADDR + 0x0], eax      ; map to same addr
+    mov [PAGE_DIR_ADDR + 0xC00], eax    ; map to kernel space
+    mov eax, PAGE_DIR_ADDR
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov [PAGE_DIR_ADDR + 4092], eax     ; map self (page directory) as page table
+
+    ; create ptes about low 1MB of physical memory
+    mov ecx, 256 ; 1MB / 4K = 256
+    mov esi, 0
+    mov edx, PG_US_U | PG_RW_W | PG_P
+.create_pte:
+    mov [ebx + esi * 4], edx
+    inc esi
+    add edx, 4096 ; a page about size 4KB
+    loop .create_pte
+
+    ; create pdes about high 1GB kernel space
+    mov eax, PAGE_TBL_ADDR
+    add eax, 0x1000 ; address of 2nd page table
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov ebx, PAGE_DIR_ADDR
+    mov ecx, 254    ; 1GB / 4MB - 2 = 254 since high 1GB has 2 pde have been set
+    mov esi, 0xC04
+.create_pde_of_kernel:
+    mov [ebx + esi], eax
+    add eax, 0x1000
+    add esi, 4
+    loop .create_pde_of_kernel
+
+    ret
 
 loader_message:
     db "Enter loader in real mode"
